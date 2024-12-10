8143c759162cf14d932eebcde1fdb8e26242f0f2
Eduardo Tanaka <tanaka.eduardo@gmail.com>
2024-12-10 11:50:10 -0300

    First step in java with springboot

---
+++ 01JavaSpringBoot/rest-spring-boot/src/test/java/com/tanaka/StartupTests.java
@@ -0,0 +1,13 @@
+package com.tanaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
